7671947faf53f49c2d2184a3c9a0bf6c65c80dd1
Lika Anjelina <likaanjelina12@gmail.com>
2024-11-21 17:48:55 +0700

    dlt

--- Codelab2/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    include "Controllers/ProductControllers.php";
-
-    use Controller\ProductController;
-
-    $productController = new ProductController;
-
-    echo $productController->getAllProduct();
